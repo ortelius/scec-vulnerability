7f62c851cda62cc2c2c43a439bce680df2dd0960
Steve Taylor <steve@deployhub.com>
2023-10-19 10:58:06 -0600

    add parameter to InitializeDB Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 var logger = database.InitLogger()
-var dbconn = database.InitializeDB()
+var dbconn = database.InitializeDB("evidence")
 
 // GetDomains godoc
 // @Summary Get a List of Domains
